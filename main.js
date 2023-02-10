b241708d5ea72e4e850b0c04a420d5114a389be2
shubham vegad <110393780+shubhamVegad@users.noreply.github.com>
2023-02-10 16:45:42 +0530

    Add files via upload
    1st commit with whole page

---
+++ main.js
@@ -0,0 +1,7 @@
+let docTitle = document.title;
+window.addEventListener("blur", ()=>{
+    document.title = "Come Back :( ";
+})
+window.addEventListener("focus", ()=>{
+    document.title = "it's shubham";
+})
